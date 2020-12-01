66a4822f6f3aa42426033c35221e85d39bec38fc
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2020-11-30 21:51:00 -0500

    Add files via upload

--- src/BookSearchBar/BookSearchBar.js
+++ src/BookSearchBar/BookSearchBar.js
@@ -13,23 +13,23 @@ class BookSearchBar extends Component {
     }
 
     render() {
-        const {handleSearchSubmit} = this.props;
-        const {searchInput} = this.state;
+        const { handleSearchSubmit } = this.props;
+        const { searchInput } = this.state;
         
         return(
             <div>
-                <div className = 'searchbar_container'>
-                    <form
-                        className = 'searchbar_form'
-                        onSubmit = {submitEvent => handleSearchSubmit(submitEvent, searchInput)}>
-                            <input
-                                className = 'searchbar_input'
-                                type = 'text'
-                                placeholder = 'Search For Books'
-                                name = 'search'
-                                onChange = {this.handleSearchSubmit}/>
-                            <button type = 'submit'><i className = 'fa fa-search search_button'/></button>
-                        </form>
+                <div className="searchbar_container">
+                    <form 
+                        className="searchbar_form"
+                        onSubmit={ submitEvent => handleSearchSubmit(submitEvent, searchInput) }>
+                        <input 
+                            className="searchbar_input" 
+                            type="text" 
+                            placeholder="Search Books ... " 
+                            name="search" 
+                            onChange={ this.handleSearchInput } />
+                        <button type="submit"><i className="fa fa-search search_button" /></button>
+                    </form>
                 </div>
             </div>
         );
